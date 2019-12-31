df703ac2b01b17af314227e7f15093862973d36d
LeopoldArkham <leopold.arkham@gmail.com>
2019-12-31 16:31:10 +0100

    Fix parsing regex

--- app/App.jsx
+++ app/App.jsx
@@ -12,12 +12,12 @@ const App = () => {
 
   
   const processChat = (raw) => {
-    const regex = /(\d{1,2}\/\d{1,2}\/\d{1,2}), (\d{2}:\d{2}) - (.+):/g;
+    const regex = /(\d{1,2}\/\d{1,2}\/\d{1,2}), (\d{2}:\d{2}) - ([^:\n\r]+): /g;
     const split = raw.split(regex);
     console.log(split)
     const removeFirst = split[0].includes("Messages to this chat and calls are now secured with end-to-end encryption");
     console.log(removeFirst);
-    const parsed = removeFirst ? split.slice(5) : split.slice(1);
+    const parsed = split.slice(1);
     const grouped = chunk(parsed, 4);
     const sender1 = grouped[0][2];
     setSender1(sender1);
