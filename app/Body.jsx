f28887d8d816d7530f188cca4433d301f491de8a
LeopoldArkham <leopold.arkham@gmail.com>
2020-05-21 23:42:48 +0200

    Add speech arrows

--- app/Body.jsx
+++ app/Body.jsx
@@ -14,8 +14,17 @@ const styles = {
     font-family: "Segoe UI";
     max-width: 66%;
     width: fit-content;
-    border-radius: 10px;
+    border-radius: 10px 0px 10px 10px;
     box-shadow: 0px 1px 0px 0px rgba(184,184,184,0.5);
+
+    &:after {
+      content: "";
+      position: absolute;
+      border-top: 13px solid #DCF8C6;
+      border-right: 10px solid transparent;
+      right: -10px;
+      top: 0;
+    }
   `,
   dateStamp: css`
     position: sticky;
@@ -36,6 +45,21 @@ const styles = {
   otherSender: css`
     margin: 10px auto 10px 10px;
     background: white;
+    border-radius: 0px 10px 10px 10px;
+
+    /* Override the arrow inherited from the base message class */
+    &:after {
+      content: none;
+    }
+
+    &:before {
+      content: "";
+      position: absolute;
+      border-top: 13px solid white;
+      border-left: 10px solid transparent;
+      left: -10px;
+      top: 0;
+    }
   `,
   timeSent: css`
     position: absolute;
