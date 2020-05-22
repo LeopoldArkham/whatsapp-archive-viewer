65a8e3e171d0e1648506a2aaf75fd25590355422
LeopoldArkham <leopold.arkham@gmail.com>
2020-05-22 11:26:44 +0200

    Only display tick on the first message

--- app/Body.jsx
+++ app/Body.jsx
@@ -14,8 +14,11 @@ const styles = {
     font-family: "Segoe UI";
     max-width: 66%;
     width: fit-content;
-    border-radius: 10px 0px 10px 10px;
+    border-radius: 10px 10px 10px 10px;
     box-shadow: 0px 1px 0px 0px rgba(184,184,184,0.5);
+  `,
+  bubbleTickBaseMessage: css`
+    border-radius: 10px 0px 10px 10px;
 
     &:after {
       content: "";
@@ -23,6 +26,23 @@ const styles = {
       border-top: 13px solid #DCF8C6;
       border-right: 10px solid transparent;
       right: -10px;
+      top: 0;
+    }
+  `,
+  bubbleTickThisSender: css`
+    border-radius: 0px 10px 10px 10px;
+
+    /* Override the arrow inherited from the base message class */
+    &:after {
+      content: none;
+    }
+
+    &:before {
+      content: "";
+      position: absolute;
+      border-top: 13px solid white;
+      border-left: 10px solid transparent;
+      left: -10px;
       top: 0;
     }
   `,
@@ -42,24 +62,9 @@ const styles = {
   padRight: css`
     padding-right: 60px 
   `,
-  otherSender: css`
+  thisSender: css`
     margin: 10px auto 10px 10px;
     background: white;
-    border-radius: 0px 10px 10px 10px;
-
-    /* Override the arrow inherited from the base message class */
-    &:after {
-      content: none;
-    }
-
-    &:before {
-      content: "";
-      position: absolute;
-      border-top: 13px solid white;
-      border-left: 10px solid transparent;
-      left: -10px;
-      top: 0;
-    }
   `,
   timeSent: css`
     position: absolute;
@@ -77,10 +82,19 @@ const Message = ({
   message,
   isSender1,
   swapSides,
+  showTick,
 }) => {
 
   return (
-    <div className={cx(styles.message, { [styles.otherSender]: isSender1 ^ swapSides, [styles.padRight]: message.length <= 115 })}>
+    <div className={cx(
+      styles.message,
+      {
+        [styles.thisSender]: isSender1 ^ swapSides,
+        [styles.bubbleTickBaseMessage]: showTick,
+        [styles.bubbleTickThisSender]: showTick && (isSender1 ^ swapSides),
+        [styles.padRight]: message.length <= 115,
+      }
+    )}>
       {message}
       <div className={styles.timeSent}>
         {time}
@@ -92,10 +106,11 @@ const Message = ({
 
 const Body = ({ chat, sender1, swapSides }) => {
   if (chat == null) return null;
+  let prevMessage = chat[1];
 
   return (
     <Pane width="1200px" margin="auto" paddingTop="70px" zIndex="0">
-      {chat.map(object => {
+      {chat.slice(0, 100).map(object => {
         if (object._type === 'date') {
           return (
             <div className={styles.dateStamp}>
@@ -110,6 +125,10 @@ const Body = ({ chat, sender1, swapSides }) => {
             message,
           } = object;
 
+          // If this message is the first of a group from the same author,
+          // it will display a speech bubble tick.
+          const isFirstOfGroup = author !== prevMessage.author;
+          prevMessage = object;
 
           return (
             <Message
@@ -118,6 +137,7 @@ const Body = ({ chat, sender1, swapSides }) => {
               message={message}
               isSender1={author === sender1}
               swapSides={swapSides}
+              showTick={isFirstOfGroup}
             />
           );
 
